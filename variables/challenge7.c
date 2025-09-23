6cc4ce1e19d5974af362af2b916c9e77c991719e
SalmaMasioub <salma.masioub6@gmail.com>
2025-09-23 19:48:15 +0100

    challenge 8

--- variables/challenge7.c
+++ variables/challenge7.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
     int main(){
-       
-        
+      float a; 
+      float b;  
+      float c;
+      float sum;
+      float resultat;
+        printf("Entrer la valeur de a: ");
+        scanf("%f", &a);
+
+        printf("Entrer la valeur de b: ");
+        scanf("%f", &b);
+
+        printf("Entrer la valeur de c: ");
+        scanf("%f", &c);
+
+        a *= 2;
+        b *= 3;
+        c *= 5;
+
+        sum = a + b + c;
+        resultat = sum / 10; // c'est 2+3+5 = 10
+
+        printf("La moyenne est: %.2f ", resultat);
+
+
+        return 0;
     }
