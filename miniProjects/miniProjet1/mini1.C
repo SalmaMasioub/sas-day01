0b27d3363417d55792ec28c8cb544f6eed5e15d2
SalmaMasioub~ <salma.masioub6@gmail.com>
2025-09-24 18:15:46 +0100

    mini projet

---
+++ miniProjects/miniProjet1/mini1.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+int menu;
+
+printf("choose option: \n 1- Ajouter un livre au stock. \n2- Afficher tous les livres disponibles. \n3-Rechercher un livre par son titre.\n4- Mettre à jour la quantité d'un livre.\n 5-Supprimer un livre du stock.\n6- Afficher le nombre total de livres en stock.\n 7- exit ");
+
+scanf("%d" ,&menu);
+
+switch(menu){
+case 1 :
+int
+
+
+break;
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
